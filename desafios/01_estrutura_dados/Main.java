4c3dcf06f22dc75e2dae264f831e868681a19727
Fabricio Guimaraes <fdcg@icomp.ufam.edu.br>
2025-05-08 10:19:39 -0400

    feat: Alterando implementação do Menor da Pilha

--- desafios/01_estrutura_dados/Main.java
+++ desafios/01_estrutura_dados/Main.java
@@ -4,25 +4,17 @@ import java.util.Scanner;
 public class Main 
 {
 
-    private Stack<Integer> pilha;
-    private int menor;
-
-    Main(){
-        this.pilha = new Stack<Integer>();
-    }
-
     public static void main(String[] args) 
     {
         Scanner leitor = new Scanner(System.in);
         int tamanho_entrada = leitor.nextInt();
         int aux;
         String operacao;
-
-        Main app = new Main();
+        Stack<Integer> pilha = new Stack<Integer>();
+        Stack<Integer> pilhaMin = new Stack<Integer>();
         
         for(int i = 0; i < tamanho_entrada; i++)
 		{
-
             operacao = leitor.next();
 
             switch(operacao) {
@@ -30,90 +22,36 @@ public class Main
 
                     aux = leitor.nextInt();
 
-                    app.empilha(aux);
+                    pilha.push(aux);
+
+                    if (pilhaMin.isEmpty() || aux <= pilhaMin.peek()){
+                        pilhaMin.push(aux);
+                    }
 
                     break;
-                
                 case "POP":
-
-                    app.desenpilha();
-
-                    break;
-
-                case "MIN":
-
-                    if (app.ehVazia()){
+                    if (pilha.isEmpty()){
                         System.out.println("EMPTY");
                     }else{
-                        app.calculaMenor();
-                        System.out.println(app.getMenor());
+                        aux = pilha.pop();
+                        if (aux == pilhaMin.peek()){
+                            pilhaMin.pop();
+                        }
+                    }
+
+                    break;
+                case "MIN":
+                    if (pilha.isEmpty()){
+                        System.out.println("EMPTY");
+                    }else{
+                        System.out.println(pilhaMin.peek());
                     }
                     break;
-                
-                default:
-                }
+            }
 
 		}
         
         leitor.close();
     }
-
-    public Stack<Integer> getPilha()
-    {
-        return this.pilha;
-    }
-
-    public void setPilha(Stack<Integer> pilha)
-    {
-        this.pilha = pilha;
-    }
-
-    public void empilha(int valor){
-        Stack<Integer> _pilha = this.getPilha();
-        
-        _pilha.push(valor);
-
-        this.setPilha(_pilha);
-    }
-
-    public int desenpilha(){
-        Stack<Integer> _pilha = this.getPilha();
-
-        int valor = _pilha.pop();
-
-        this.setPilha(_pilha);
-
-        return valor;
-    }
-
-    public boolean ehVazia(){
-        Stack<Integer> _pilha = this.getPilha();
-
-        return _pilha.isEmpty();
-    }
-
-    public int getMenor(){
-        return this.menor;
-    }
-
-    public void setMenor(int menor){
-        this.menor = menor;
-    }
-
-    public void calculaMenor(){
-        Stack<Integer> _pilha = this.getPilha();
-
-        int menor = _pilha.peek();
-
-        for (int valor : _pilha) {
-            if (valor < menor) {
-                menor = valor;
-            }
-        }
-
-        this.setMenor(menor);
-    }
-
-
 
 }
